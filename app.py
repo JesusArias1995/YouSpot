7f68a56d785284c99e2e2ae72841cfbe1bcb67eb
Jesús Arias <jesusariasgil1995@hotmail.com>
2018-05-23 13:27:42 +0200

    Modificaciones

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def videoslista(videoid):
 			lista_id.append(x['snippet']['resourceId']['videoId'])
 			lista_ti.append(x['snippet']['title'])
 		if len(lista_id) != 0:
-			return render_template('cancioneslistasyt.html', lista_id=lista_id, lista_ti=lista_ti, buscar=buscar)
+			return render_template('cancioneslistasyt.html', lista_id=lista_id, lista_ti=lista_ti)
 
 if __name__ == '__main__':
 	port=os.environ["PORT"]
