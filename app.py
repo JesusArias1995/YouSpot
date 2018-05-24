3b9e563bb0c2771419e4d70dd8ebe3d619a5b292
Jesús Arias <jesusariasgil1995@hotmail.com>
2018-05-24 12:29:30 +0200

    Modificado

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ def añadircancionplaylist(idc, uri):
 		payload={'uris':uri}
 		r = oauth2.post('https://api.spotify.com/v1/users/{}/playlists/{}/tracks' .format(session["id"], idc), params=payload, headers=headers)
 		doc=json.loads(r.content.decode("utf-8"))
-		return render_template("misplaylist.html")
+		return render_template("cancionañadida.html", datos=doc)
 	else:
 		return redirect('/')
 
